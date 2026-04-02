fe3509f17bce6fac8dc1bf3366534f94be757eb0
Scan-neuroscan360 <scan.iitb2022@gmail.com>
2026-04-02 17:18:17 +0530

    ss

---
+++ Assets/1st Project Files/Scripts/Inverter.cs
@@ -0,0 +1,22 @@
+﻿public class Inverter : Node
+{
+   public Inverter(string nodeName)
+   {
+       NodeName = nodeName;
+   }
+   
+   public override NodeStatus Process()
+   {
+       var childStatus = Children[CurrentChild].Process();
+        
+       switch (childStatus)
+       {
+           case NodeStatus.Running:
+               return NodeStatus.Running;
+           case NodeStatus.Failure:
+               return NodeStatus.Success;
+       }
+
+         return NodeStatus.Failure;
+   }
+}
