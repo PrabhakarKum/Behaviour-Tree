eee27dc14120a791a203e2122ec0eb976ad55500
Prabhakar <prabhakar.iitbombay@gmail.com>
2026-04-04 12:43:34 +0530

    ss

--- Assets/1st Project Files/Scripts/BTAgent.cs
+++ Assets/1st Project Files/Scripts/BTAgent.cs
@@ -5,139 +5,49 @@ using VHierarchy.Libs;
 
 public class BTAgent : MonoBehaviour
 { 
-    private BehaviourTree _tree;
-    public GameObject frontDoor;
-    public GameObject backDoor;
-    public GameObject diamond;
-    public GameObject van;
-    
-    [Range(0,1000)]
-    public int money = 800;
-    
-    private NavMeshAgent _agent;
+    protected BehaviourTree _tree;
+    protected NavMeshAgent _agent;
 
-    private enum ActionState { Idle, Working };
-    private ActionState _actionState = ActionState.Idle;
-    
-    private Node.NodeStatus _treeStatus = Node.NodeStatus.Running;
-    private void Start()
+    protected enum ActionState
+    {
+        Idle,
+        Working
+    };
+
+    protected ActionState _actionState = ActionState.Idle;
+    protected Node.NodeStatus _treeStatus = Node.NodeStatus.Running;
+
+    protected virtual void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        
         _tree = new BehaviourTree(name);
-        
-        var steal = new Sequence("Steal Something");
-        
-        var hasGotMoney = new Leaf("Has Got Money", HasMoney);
-        
-        var openDoor = new Selector("Open Door");
-        
-        var invertMoney = new Inverter("Invert Money");
-        
-        var goToFrontDoor = new Leaf("Go To Front Door", GoToFrontDoor);
-        var goToBackDoor =  new Leaf("Go To Back Door", GoToBackDoor);
-        
-        var goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
-        
-        var goToVan  = new Leaf("Go To Van", GoToVan);
-        
-        invertMoney.AddChild(hasGotMoney);
-        steal.AddChild(invertMoney);
-        
-        openDoor.AddChild(goToFrontDoor);
-        openDoor.AddChild(goToBackDoor);
-        
-        steal.AddChild(openDoor);
-        steal.AddChild(goToDiamond);
-        steal.AddChild(goToVan);
-        
-        _tree.AddChild(steal);
-        _tree.PrintTree();
-    }
-    
-    private Node.NodeStatus HasMoney()
-    {
-        return money < 500 ? Node.NodeStatus.Failure : Node.NodeStatus.Success;
-    }
-    
-    private Node.NodeStatus GoToFrontDoor()
-    {
-        return GoToDoor(frontDoor);
-    }
-    
-    private Node.NodeStatus GoToBackDoor()
-    {
-        return GoToDoor(backDoor);
     }
 
-    private Node.NodeStatus GoToDiamond()
+    protected Node.NodeStatus GoToLocation(Vector3 destination)
     {
-       var status =  GoToLocation(diamond.transform.position);
-       
-       if (status == Node.NodeStatus.Success)
-       {
-           diamond.transform.SetParent(gameObject.transform);
-       }
+        var distance = Vector3.Distance(transform.position, destination);
 
-       return status;
-    }
-    
-    private Node.NodeStatus GoToVan()
-    {
-       var status =  GoToLocation(van.transform.position);
-       
-       if (status == Node.NodeStatus.Success)
-       {
-           diamond.Destroy(); 
-           money += 300;
-       }
-
-       return status;
-    }
-
-    public Node.NodeStatus GoToDoor(GameObject door)
-    {
-        var status = GoToLocation(door.transform.position);
-        
-        if (status == Node.NodeStatus.Success)
+        if (_actionState == ActionState.Idle)
         {
-            if (!door.GetComponent<Lock>().IsLocked)
-            {
-                door.SetActive(false);
-                return Node.NodeStatus.Success;
-            }
+            _agent.SetDestination(destination);
+            _actionState = ActionState.Working;
+        }
+        else if (Vector3.Distance(_agent.pathEndPosition, destination) >= 2)
+        {
+            _actionState = ActionState.Idle;
             return Node.NodeStatus.Failure;
         }
+        else if (distance <= 2)
+        {
+            _actionState = ActionState.Idle;
+            return Node.NodeStatus.Success;
+        }
 
-        return status;
-    }
-    
-    private Node.NodeStatus GoToLocation(Vector3 destination)
-    {
-       var distance = Vector3.Distance(transform.position, destination);
-       
-       if (_actionState == ActionState.Idle)
-       {
-           _agent.SetDestination(destination);
-           _actionState = ActionState.Working;
-       }
-       else if (Vector3.Distance(_agent.pathEndPosition, destination) >= 2)
-       {
-           _actionState = ActionState.Idle;
-           return Node.NodeStatus.Failure;
-       }
-       else if (distance <= 2)
-       {
-           _actionState = ActionState.Idle;
-           return Node.NodeStatus.Success;
-       }
-       
-       return Node.NodeStatus.Running;
+        return Node.NodeStatus.Running;
     }
 
-    private void Update()
+    protected virtual void Update()
     {
-        if(_treeStatus != Node.NodeStatus.Success)
-            _treeStatus = _tree.Process();
+        if (_treeStatus != Node.NodeStatus.Success) _treeStatus = _tree.Process();
     }
 }
--- Assets/1st Project Files/Scripts/RobberBehaviour.cs
+++ Assets/1st Project Files/Scripts/RobberBehaviour.cs
@@ -3,64 +3,58 @@ using UnityEngine;
 using UnityEngine.AI;
 using VHierarchy.Libs;
 
-public class RobberBehaviour : MonoBehaviour
-{ 
-    private BehaviourTree _tree;
+public class RobberBehaviour : BTAgent
+{
     public GameObject frontDoor;
     public GameObject backDoor;
     public GameObject diamond;
     public GameObject van;
-    
-    [Range(0,1000)]
-    public int money = 800;
-    
-    private NavMeshAgent _agent;
 
-    private enum ActionState { Idle, Working };
-    private ActionState _actionState = ActionState.Idle;
-    
-    private Node.NodeStatus _treeStatus = Node.NodeStatus.Running;
-    private void Start()
+    [Range(0, 1000)] public int money = 800;
+
+    protected override void Start()
     {
-        _agent = GetComponent<NavMeshAgent>();
-        
-        _tree = new BehaviourTree(name);
-        
+        base.Start();
+
         var steal = new Sequence("Steal Something");
-        
+
         var hasGotMoney = new Leaf("Has Got Money", HasMoney);
-        
+
         var openDoor = new Selector("Open Door");
+
+        var invertMoney = new Inverter("Invert Money");
+
         var goToFrontDoor = new Leaf("Go To Front Door", GoToFrontDoor);
-        var goToBackDoor =  new Leaf("Go To Back Door", GoToBackDoor);
-        
+        var goToBackDoor = new Leaf("Go To Back Door", GoToBackDoor);
+
         var goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
-        
-        var goToVan  = new Leaf("Go To Van", GoToVan);
-        
-        steal.AddChild(hasGotMoney);
-        
+
+        var goToVan = new Leaf("Go To Van", GoToVan);
+
+        invertMoney.AddChild(hasGotMoney);
+        steal.AddChild(invertMoney);
+
         openDoor.AddChild(goToFrontDoor);
         openDoor.AddChild(goToBackDoor);
-        
+
         steal.AddChild(openDoor);
         steal.AddChild(goToDiamond);
         steal.AddChild(goToVan);
-        
+
         _tree.AddChild(steal);
         _tree.PrintTree();
     }
-    
+
     private Node.NodeStatus HasMoney()
     {
-        return money > 500 ? Node.NodeStatus.Failure : Node.NodeStatus.Success;
+        return money < 500 ? Node.NodeStatus.Failure : Node.NodeStatus.Success;
     }
-    
+
     private Node.NodeStatus GoToFrontDoor()
     {
         return GoToDoor(frontDoor);
     }
-    
+
     private Node.NodeStatus GoToBackDoor()
     {
         return GoToDoor(backDoor);
@@ -68,33 +62,33 @@ public class RobberBehaviour : MonoBehaviour
 
     private Node.NodeStatus GoToDiamond()
     {
-       var status =  GoToLocation(diamond.transform.position);
-       
-       if (status == Node.NodeStatus.Success)
-       {
-           diamond.transform.SetParent(gameObject.transform);
-       }
+        var status = GoToLocation(diamond.transform.position);
 
-       return status;
+        if (status == Node.NodeStatus.Success)
+        {
+            diamond.transform.SetParent(gameObject.transform);
+        }
+
+        return status;
     }
-    
+
     private Node.NodeStatus GoToVan()
     {
-       var status =  GoToLocation(van.transform.position);
-       
-       if (status == Node.NodeStatus.Success)
-       {
-           diamond.Destroy(); 
-           money += 300;
-       }
+        var status = GoToLocation(van.transform.position);
 
-       return status;
+        if (status == Node.NodeStatus.Success)
+        {
+            diamond.Destroy();
+            money += 300;
+        }
+
+        return status;
     }
 
     public Node.NodeStatus GoToDoor(GameObject door)
     {
         var status = GoToLocation(door.transform.position);
-        
+
         if (status == Node.NodeStatus.Success)
         {
             if (!door.GetComponent<Lock>().IsLocked)
@@ -102,38 +96,15 @@ public class RobberBehaviour : MonoBehaviour
                 door.SetActive(false);
                 return Node.NodeStatus.Success;
             }
+
             return Node.NodeStatus.Failure;
         }
 
         return status;
     }
-    
-    private Node.NodeStatus GoToLocation(Vector3 destination)
-    {
-       var distance = Vector3.Distance(transform.position, destination);
-       
-       if (_actionState == ActionState.Idle)
-       {
-           _agent.SetDestination(destination);
-           _actionState = ActionState.Working;
-       }
-       else if (Vector3.Distance(_agent.pathEndPosition, destination) >= 2)
-       {
-           _actionState = ActionState.Idle;
-           return Node.NodeStatus.Failure;
-       }
-       else if (distance <= 2)
-       {
-           _actionState = ActionState.Idle;
-           return Node.NodeStatus.Success;
-       }
-       
-       return Node.NodeStatus.Running;
-    }
 
-    private void Update()
+    protected override void Update()
     {
-        if(_treeStatus != Node.NodeStatus.Success)
-            _treeStatus = _tree.Process();
+        base.Update();
     }
 }
